8922500da99bf655669b6460e003b810aa0cf1fd
DidierDev672 <ariasdev672@gmail.com>
2022-11-17 09:38:58 -0500

    Service of our product data logics

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,40 +1,44 @@
-import { ProductEntry, NonSensitiveInfoProductEntry,NewProductEntry } from "../types";
-import productsData from "./products.json";
+import Product, { IProduct } from "../models/Product";
+import { ObjectId } from "mongodb";
 
-const products: ProductEntry[] = productsData as ProductEntry[];
-
-export const getEntries = (): ProductEntry[] => products;
-
-export const findById = (id:number): NonSensitiveInfoProductEntry | undefined => {
-    const entry = products.find(d => d.id === id);
-    if(entry != null){
-        const { description, ...restOfProduct } = entry;
-        return restOfProduct;
+export async function getProductsServices(){
+    try{
+        return await Product.find();
+    }catch(error){
+        console.error(error);
     }
-
-    return undefined;
 };
 
-export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoProductEntry[] => {
-    return products.map(({ id, quantity, price, name_product, category, description, date_entry  }) => {
-        return {
-            id,
-            quantity,
-            price,
-            name_product,
-            category,
-            description,
-            date_entry
-        }
-    })
-};
+export async function getProductServices(id: ObjectId){
+    try{
+        return await Product.findOne({ _id: id });
+    }catch (error){
+        console.error(error);
+    }
+}
 
-export const addProduct = (newProductEntry: NewProductEntry): ProductEntry => {
-    const newProduct = {
-        id: Math.max(...products.map(d => d.id)) + 1,
-        ...newProductEntry
-    };
+export async function createProductServices(product: IProduct){
+    try{
+        await product.save();
+        return product;
+    }catch(error){
+        console.error(error);
+    }
+}
 
-    products.push(newProduct);
-    return newProduct;
-};
+export async function updateProductServices(id: ObjectId, product: IProduct){
+    try{
+        await Product.findByIdAndUpdate({ _id: id }, product);
+        return product;
+    }catch(error){
+        console.error(error);
+    }
+}
+
+export async function deleteProductService(id: ObjectId){
+    try{
+        return await Product.findByIdAndDelete({ _id: id });
+    }catch(error){
+        console.error(error)
+    }
+}
